0649c1caf307437cdcf0c4e56ce71be920a952d0
kelvinh111 <kelvinh111.register@gmail.com>
2024-10-24 21:18:57 +0100

    feat: added sky shader

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -7,11 +7,34 @@ import portalFragmentShader from './shaders/portal/fragment.glsl'
 import * as THREE from 'three';
 import { useRef } from 'react';
 
+const loader = new THREE.TextureLoader();
+const texture0 = loader.load("./4.png");
+texture0.minFilter = THREE.NearestFilter;
+texture0.magFilter = THREE.NearestFilter;
+texture0.wrapS = THREE.RepeatWrapping;
+texture0.wrapT = THREE.RepeatWrapping;
+
+
+const materialParameters = {
+    sunColor: { r: 1.09, g: 0.746, b: 1.828 },
+    lightColor: { r: 0.918, g: 2, b: 2 },
+    darkColor: { r: 0, g: 0.299, b: 0.73 },
+    baseSkyColor: { r: 0.607, g: 0.557, b: 1 },
+};
+
 const PortalMaterial = shaderMaterial(
     {
-        uTime: 0,
-        uColorStart: new THREE.Color('#ffffff'),
-        uColorEnd: new THREE.Color('#000000')
+        // uTime: 0,
+        // uColorStart: new THREE.Color(1,0,1),
+        // uColorEnd: new THREE.Color(0,0,0),
+        iTime: 0,
+        iMouse: new THREE.Vector4(),
+        iResolution: new THREE.Vector3(),
+        iChannel0: texture0,
+        uSunColor: new THREE.Color(materialParameters.sunColor.r, materialParameters.sunColor.g, materialParameters.sunColor.b),
+        uLightColor: new THREE.Color(materialParameters.lightColor.r, materialParameters.lightColor.g, materialParameters.lightColor.b),
+        uDarkColor: new THREE.Color(materialParameters.darkColor.r, materialParameters.darkColor.g, materialParameters.darkColor.b),
+        uBaseSkyColor: new THREE.Color(materialParameters.baseSkyColor.r, materialParameters.baseSkyColor.g, materialParameters.baseSkyColor.b),
     },
     portalVertexShader,
     portalFragmentShader
@@ -20,20 +43,21 @@ const PortalMaterial = shaderMaterial(
 extend({ PortalMaterial })
 
 const objectNames = [
-    "book", "chair2", "curtain", "coffee", "window1", "window2", 
-    "computer", "desk_lamp", "ceiling", "wall1", "wall2", "floor", 
-    "desk", "keyboard", "mouse", "plant1", "plant2", "plant3", 
+    "book", "chair2", "curtain", "coffee", "window1", "window2",
+    "computer", "desk_lamp", "ceiling", "wall1", "wall2", "floor",
+    "desk", "keyboard", "mouse", "plant1", "plant2", "plant3",
     "floor_lamp", "curtain_stick", "poster1", "poster2", "poster3", "phone"
 ];
+
 
 const glasses = ['window1glass1', 'window1glass2', 'window2glass1', 'window2glass2'];
 
 export default function Experience() {
     const normalMap = useTexture("./model/dirt1.png");
     normalMap.wrapS = normalMap.wrapT = 1000;
-    
+
     const buffer = useFBO();
-    
+
     // References for glass, scene, and plane objects
     const sceneRef = useRef();
     const glassRefs = useRef([]);
@@ -54,18 +78,20 @@ export default function Experience() {
     });
 
     // Use useFrame to handle rendering order
+
     useFrame((state, delta) => {
-        portalMaterial.current.uTime += delta
+        portalMaterial.current.iResolution.set(window.innerWidth*2, window.innerHeight*1.5, 1)
+        portalMaterial.current.iTime += delta
 
         planeRef.current.visible = true;
         // Hide the glass during the buffer render
         glassRefs.current.forEach(ref => ref.visible = false);
-        
+
         // Render the scene (including the plane) into the FBO buffer
         state.gl.setRenderTarget(buffer);
         state.gl.render(state.scene, state.camera);
         state.gl.setRenderTarget(null);
-        
+
         planeRef.current.visible = false;
         // Show the glass after the buffer is rendered
         glassRefs.current.forEach(ref => ref.visible = true);
@@ -74,14 +100,14 @@ export default function Experience() {
     return (
         <>
             <OrbitControls makeDefault target={[-3.2, 1.4, -3.5]} />
-            
+
             {/* All non-glass objects, including the plane */}
             <group ref={sceneRef}>
                 {/* Add the plane (which should be visible through the glass) */}
                 <mesh rotation={[0, 0, 0]} position={[0, 1, -5]} ref={planeRef}>
                     <planeGeometry args={[10, 10]} /> {/* Width, height */}
                     {/* <meshStandardMaterial color="#5500cc" /> */}
-                    <portalMaterial ref={ portalMaterial } />
+                    <portalMaterial ref={portalMaterial} />
                 </mesh>
 
                 {/* Non-glass objects */}
@@ -94,11 +120,11 @@ export default function Experience() {
 
             {/* Glass objects */}
             {glasses.map((name, index) => (
-                <mesh 
-                    key={name} 
-                    geometry={nodes[name]?.geometry} 
-                    position={nodes[name]?.position} 
-                    rotation={nodes[name]?.rotation} 
+                <mesh
+                    key={name}
+                    geometry={nodes[name]?.geometry}
+                    position={nodes[name]?.position}
+                    rotation={nodes[name]?.rotation}
                     ref={ref => (glassRefs.current[index] = ref)} // Store the reference
                 >
                     <MeshTransmissionMaterial
@@ -120,10 +146,9 @@ export default function Experience() {
             {/* <Environment background near={1} far={1000} resolution={256}>
                 <mesh scale={100}>
                     <sphereGeometry args={[1, 64, 64]} />
-                    <meshBasicMaterial map={envMap} side={THREE.BackSide} />
+                    <meshBasicMaterial color="black" side={THREE.BackSide} />
                 </mesh>
             </Environment> */}
-
 
             <Sparkles
                 size={2}
--- src/shaders/portal/fragment.glsl
+++ src/shaders/portal/fragment.glsl
@@ -1,106 +1,71 @@
-uniform float uTime;
-uniform vec3 uColorStart;
-uniform vec3 uColorEnd;
+uniform vec3 iResolution;
+uniform float iTime;
+uniform vec4 iMouse;
+uniform sampler2D iChannel0;
+
+uniform vec3 uSunColor;
+uniform vec3 uLightColor;
+uniform vec3 uDarkColor;
+uniform vec3 uBaseSkyColor;
 
 varying vec2 vUv;
 
-//    Classic Perlin 3D Noise 
-//    by Stefan Gustavson
-//
-vec4 permute(vec4 x){ return mod(((x*34.0)+1.0)*x, 289.0); }
-vec4 taylorInvSqrt(vec4 r){ return 1.79284291400159 - 0.85373472095314 * r; }
-vec3 fade(vec3 t) { return t*t*t*(t*(t*6.0-15.0)+10.0); }
-
-float cnoise(vec3 P)
-{
-    vec3 Pi0 = floor(P); // Integer part for indexing
-    vec3 Pi1 = Pi0 + vec3(1.0); // Integer part + 1
-    Pi0 = mod(Pi0, 289.0);
-    Pi1 = mod(Pi1, 289.0);
-    vec3 Pf0 = fract(P); // Fractional part for interpolation
-    vec3 Pf1 = Pf0 - vec3(1.0); // Fractional part - 1.0
-    vec4 ix = vec4(Pi0.x, Pi1.x, Pi0.x, Pi1.x);
-    vec4 iy = vec4(Pi0.yy, Pi1.yy);
-    vec4 iz0 = Pi0.zzzz;
-    vec4 iz1 = Pi1.zzzz;
-
-    vec4 ixy = permute(permute(ix) + iy);
-    vec4 ixy0 = permute(ixy + iz0);
-    vec4 ixy1 = permute(ixy + iz1);
-
-    vec4 gx0 = ixy0 / 7.0;
-    vec4 gy0 = fract(floor(gx0) / 7.0) - 0.5;
-    gx0 = fract(gx0);
-    vec4 gz0 = vec4(0.5) - abs(gx0) - abs(gy0);
-    vec4 sz0 = step(gz0, vec4(0.0));
-    gx0 -= sz0 * (step(0.0, gx0) - 0.5);
-    gy0 -= sz0 * (step(0.0, gy0) - 0.5);
-
-    vec4 gx1 = ixy1 / 7.0;
-    vec4 gy1 = fract(floor(gx1) / 7.0) - 0.5;
-    gx1 = fract(gx1);
-    vec4 gz1 = vec4(0.5) - abs(gx1) - abs(gy1);
-    vec4 sz1 = step(gz1, vec4(0.0));
-    gx1 -= sz1 * (step(0.0, gx1) - 0.5);
-    gy1 -= sz1 * (step(0.0, gy1) - 0.5);
-
-    vec3 g000 = vec3(gx0.x,gy0.x,gz0.x);
-    vec3 g100 = vec3(gx0.y,gy0.y,gz0.y);
-    vec3 g010 = vec3(gx0.z,gy0.z,gz0.z);
-    vec3 g110 = vec3(gx0.w,gy0.w,gz0.w);
-    vec3 g001 = vec3(gx1.x,gy1.x,gz1.x);
-    vec3 g101 = vec3(gx1.y,gy1.y,gz1.y);
-    vec3 g011 = vec3(gx1.z,gy1.z,gz1.z);
-    vec3 g111 = vec3(gx1.w,gy1.w,gz1.w);
-
-    vec4 norm0 = taylorInvSqrt(vec4(dot(g000, g000), dot(g010, g010), dot(g100, g100), dot(g110, g110)));
-    g000 *= norm0.x;
-    g010 *= norm0.y;
-    g100 *= norm0.z;
-    g110 *= norm0.w;
-    vec4 norm1 = taylorInvSqrt(vec4(dot(g001, g001), dot(g011, g011), dot(g101, g101), dot(g111, g111)));
-    g001 *= norm1.x;
-    g011 *= norm1.y;
-    g101 *= norm1.z;
-    g111 *= norm1.w;
-
-    float n000 = dot(g000, Pf0);
-    float n100 = dot(g100, vec3(Pf1.x, Pf0.yz));
-    float n010 = dot(g010, vec3(Pf0.x, Pf1.y, Pf0.z));
-    float n110 = dot(g110, vec3(Pf1.xy, Pf0.z));
-    float n001 = dot(g001, vec3(Pf0.xy, Pf1.z));
-    float n101 = dot(g101, vec3(Pf1.x, Pf0.y, Pf1.z));
-    float n011 = dot(g011, vec3(Pf0.x, Pf1.yz));
-    float n111 = dot(g111, Pf1);
-
-    vec3 fade_xyz = fade(Pf0);
-    vec4 n_z = mix(vec4(n000, n100, n010, n110), vec4(n001, n101, n011, n111), fade_xyz.z);
-    vec2 n_yz = mix(n_z.xy, n_z.zw, fade_xyz.y);
-    float n_xyz = mix(n_yz.x, n_yz.y, fade_xyz.x); 
-
-    return 2.2 * n_xyz;
+vec3 gamma(vec3 col, float g) {
+    return pow(col, vec3(g));
 }
 
-void main()
-{
-    // Displace the UV
-    vec2 displacedUv = vUv + cnoise(vec3(vUv * 5.0, uTime * 0.1));
+float noiseLayer(vec2 uv) {
+    float t = (iTime + iMouse.x) / 5.;
+    uv.y -= t / 60.;
+    float e = 0.;
+    for(float j = 1.; j < 9.; j++) {
+        float timeOffset = t * mod(j, 2.989) * .02 - t * .015;
+        e += 1. - texture(iChannel0, uv * (j * 1.789) + j * 159.45 + timeOffset).r / j;
+    }
+    e /= 3.5;
+    return e;
+}
 
-    // Perlin noise
-    float strength = cnoise(vec3(displacedUv * 5.0, uTime * 0.2));
+vec3 drawSky(vec2 uv, vec2 uvInit) {
+    float clouds = noiseLayer(uv);
+    float eps = 0.1;
+    vec3 n = vec3(clouds - noiseLayer(uv + vec2(eps, 0.)), -.3, clouds - noiseLayer(uv + vec2(0., eps)));
+    n = normalize(n);
+    float l = dot(n, normalize(vec3(uv.x, -.2, uv.y + .5)));
+    l = clamp(l, 0., 1.);
 
-    // Outer glow
-    float outerGlow = distance(vUv, vec2(0.5)) * 5.0 - 1.4;
-    strength += outerGlow;
+    vec3 cloudColor = mix(uBaseSkyColor, uDarkColor, length(uvInit) * 1.7);
+    cloudColor = mix(cloudColor, uSunColor, l);
 
-    // Apply cool step
-    strength += step(- 0.2, strength) * 0.8;
+    vec3 skyColor = mix(uLightColor, uBaseSkyColor, clamp(uvInit.y * 2., 0., 1.));
+    skyColor = mix(skyColor, uDarkColor, clamp(uvInit.y * 3. - .8, 0., 1.));
+    skyColor = mix(skyColor, uSunColor, clamp(-uvInit.y * 10. + 1.1, 0., 1.));
 
-    // // Clamp the value from 0 to 1
-    // strength = clamp(strength, 0.0, 1.0);
+    if(length(uvInit - vec2(0., .04)) < .03) {
+        skyColor += vec3(2., 1., .8);
+    }
 
-    // Final color
-    vec3 color = mix(uColorStart, uColorEnd, strength);
+    float cloudMix = clamp(0., 1., clouds * 4. - 8.);
+    vec3 color = mix(cloudColor, skyColor, clamp(cloudMix, 0., 1.));
 
-    gl_FragColor = vec4(color, 1.0);
+    return color;
+}
+
+void main() {
+    vec2 uvInit = gl_FragCoord.xy / iResolution.xy;
+    uvInit.x -= .5;
+    uvInit.x *= iResolution.x / iResolution.y;
+    vec2 uv = uvInit;
+    uv.y -= .01;
+    uv.y = abs(uv.y);
+    uv.y = log(uv.y) / 2.;
+    uv.x *= 1. - uv.y;
+    uv *= .2;
+
+    vec3 col = drawSky(uv, uvInit);
+    col += vec3(1., .8, .6) * (0.55 - length(uvInit));
+    col *= .75;
+    col = gamma(col, 1.3);
+
+    gl_FragColor = vec4(col, 1.);
 }
